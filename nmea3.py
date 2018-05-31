dfaf2de7815e32681971df47435ba9d03428213c
Elagin Pavel <elagin.pasha@gmail.com>
2018-05-31 18:38:54 +0300

    Увеличено количество обрабатываемых полей.

--- nmea3.py
+++ nmea3.py
@@ -21,22 +21,30 @@ def getGPXEnder():
 	return segEnd
 
 def getGPXPoint(line):
-	#print 'getGPXPoint'
+	if line.find('$', 1) != -1: #double message on one string
+		return ''
 	msg = pynmea2.parse(line)
 	try:
 		time = getTime(msg)
+		#print time
+		if not hasattr(msg, 'latitude') or not hasattr(msg, 'longitude'):
+			return ''
+
 		if msg.latitude > 0 or msg.longitude > 0:
+			
 			#if 'writeName' in globals():
 			trkptStart = '\r<trkpt lat="' + str(msg.latitude) + '" lon="' + str(msg.longitude) + '">'
 			trkptEnd = '\r</trkpt>'
+			#time = '-'
 			res = trkptStart + time
 			if hasattr(msg, 'num_sats'):
 				sats = '\r\t<sat>' + str(msg.num_sats) + '</sat>'
 				res = res + sats
+				
 			if hasattr(msg, 'altitude'):
 				ele = '\r\t<ele>' + str(msg.altitude) + '</ele>'
 				res = res + ele
-				
+
 			if hasattr(msg, 'spd_over_grnd'):
 				speed = '\r\t<speed>' + str(msg.spd_over_grnd) + '</speed>'
 				res = res + speed
@@ -44,19 +52,56 @@ def getGPXPoint(line):
 			if hasattr(msg, 'true_course'):
 				course = '\r\t<course>' + str(msg.true_course) + '</course>'
 				res = res + course
-			#ele = '\r\t<ele>' + str(msg.altitude) + '</ele>'
+				
+			if hasattr(msg, 'data_status'):
+				data_status = '\r\t<data_status>' + str(msg.data_status) + '</data_status>'
+				res = res + data_status
+				
+			if hasattr(msg, 'variation'):
+				variation = '\r\t<variation>' + str(msg.variation) + '</variation>'
+				res = res + variation
+
+			if hasattr(msg, 'var_dir'):
+				var_dir = '\r\t<var_dir>' + str(msg.var_dir) + '</var_dir>'
+				res = res + var_dir
+
+			if hasattr(msg, 'status'):
+				status = '\r\t<status>' + str(msg.status) + '</status>'
+				res = res + status
+				
+			if hasattr(msg, 'mag_variation'):
+				mag_variation = '\r\t<mag_variation>' + str(msg.mag_variation) + '</mag_variation>'
+				res = res + mag_variation
+				
+			if hasattr(msg, 'mag_var_dir'):
+				mag_var_dir = '\r\t<mag_var_dir>' + str(msg.mag_var_dir) + '</mag_var_dir>'
+				res = res + mag_var_dir
+				
+			if hasattr(msg, 'gps_qual'):
+				gps_qual = '\r\t<gps_qual>' + str(msg.gps_qual) + '</gps_qual>'
+				res = res + gps_qual
+				
+			#if hasattr(msg, 'azimuth_1'):
+				#azimuth_1 = '\r\t<azimuth_1>' + str(msg.azimuth_1) + '</azimuth_1>'
+				#res = res + azimuth_1
+				
+			if hasattr(msg, 'spd_over_grnd_kmph'):
+				spd_over_grnd_kmph = '\r\t<spd_over_grnd_kmph>' + str(msg.spd_over_grnd_kmph) + '</spd_over_grnd_kmph>'
+				res = res + spd_over_grnd_kmph
 			res = res + trkptEnd
-			#+ sats + ele + trkptEnd
 			return str(res)
 	except AttributeError as error:
 		print 'Attribute'
+		print line
 	return str('')
 	
 def getTime(msg):
 	#print 'getTime'
 	global datestampGlobal
 	try:
-		datestampGlobal = msg.datestamp
+		if hasattr(msg, 'datestamp'):
+			datestampGlobal = msg.datestamp
+		#datestampGlobal = msg.datestamp
 	except AttributeError as error:
 		pass
 		#print 'datestamp not found'
@@ -95,7 +140,7 @@ def main():
 						file = open(writeName, 'a')
 						file.write(getGPXPoint(line))
 						file.close()
-					continue
+					#continue
 				elif '$GPRMC' in line and len(line) > 59:
 					#print line
 					#if 'writeName' in globals():
@@ -111,7 +156,7 @@ def main():
 						#file.write(str(getGPXHeader()))
 						#file.close()
 					#print "=================="
-					continue
+					#continue
 				elif '$GPGLL' in line:
 					#print line
 					#msg = pynmea2.parse(line)
@@ -123,26 +168,31 @@ def main():
 						file.close()					
 					#print msg.datestamp.strftime("%Y-%m-%d") + msg.timestamp.strftime("T%H:%M:%SZ")
 					#print msg.timestamp
-					continue
+					#continue
 				elif 'BB' in line:
 					continue
 				elif 'GPVTG' in line: #Track Made Good and Ground Speed. 
+					#if len(writeName) > 0:
+					#	file = open(writeName, 'a')
+					#	point = getGPXPoint(line)
+					#	file.write(point)
+					#	file.close()					
 					#print line
 					continue
 				elif 'GPGSV' in line: #GPS Satellites in View
-					#print line
-					continue
+					if len(writeName) > 0:
+						#msg = pynmea2.parse(line)
+						#print msg
+						#file = open(writeName, 'a')
+						#point = getGPXPoint(line)
+						#file.write(point)
+						#file.close()					
+						continue
 				elif 'GPGSA' in line: #GPS DOP and active satellites 
 					#print line
 					continue
 				elif 'GPTXT' in line:
 					#print line
-					continue
-				elif 'GPRMC' in line: #Recommended minimum specific GPS/Transit data 
-					#print line
-					msg = pynmea2.parse(line)
-					#print '$GPRMC'
-					#print msg.timestamp			
 					continue
 				else:
 					#print line
